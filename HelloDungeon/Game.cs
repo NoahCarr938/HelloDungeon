487c92e082c7108f43c29c6aba43dcd5a002387b
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2024-08-28 13:03:28 -0500

    Added player classes, weapons, and armor.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -14,6 +15,8 @@ namespace HelloDungeon
             string playername = "Bobilus";
             float playerHealth = 10.0f;
             float playerMana = 5.0f;
+            string playerWeapon = "";
+            string playerArmor = "";
             int playerGold = 3;
             float playerStamina = 20.0f;
             float playerReputation = 0.0f;
@@ -22,7 +25,10 @@ namespace HelloDungeon
             int staminaPotion = 1;
             int healthPotion = 1;
             int playerScore = 0;
+            string playerRole = "Warrior";
+            string playerRole2 = "Wizard";
             bool isDead = false;
+            
 
 
             Console.WriteLine("Hello, " + playername);
@@ -33,6 +39,7 @@ namespace HelloDungeon
             Console.WriteLine("Health: " + playerHealth);
             Console.WriteLine("Mana: " + playerMana);
             Console.WriteLine("Stamina: " + playerStamina);
+            Console.WriteLine("Current Weapon: " + playerWeapon);
             Console.WriteLine("Mana Potion: " + manaPotion);
             Console.WriteLine("Stamina Potion: " + staminaPotion);
             Console.WriteLine("Health Potion: " + healthPotion);
@@ -40,8 +47,31 @@ namespace HelloDungeon
             Console.WriteLine("Reputation: " + playerReputation);
             Console.WriteLine("Party Members: " + partyMembers);
             Console.WriteLine();
-            Console.WriteLine("You find yourself in a generic dungeon with two doors what door do you choose?");
-         
+            Console.WriteLine("Are you a warrior or a wizard.");
+
+            string input = "";
+            
+
+            while (input != "1" && input != "2") 
+            {
+                Console.Write("(1 | 2) > ");    
+                input = Console.ReadLine();
+            }
+            if (input == "1")
+            {
+                playerRole = "Warrior";
+                playerWeapon = "Sword";
+                playerArmor = "Heavy";
+            }
+            else if (input == "2")
+            {
+                playerRole = "Wizard";
+                playerWeapon = "Staff";
+                playerArmor = "Light";
+            }
+            Console.WriteLine("Player role: " + playerRole);
+            Console.WriteLine("Player Weapon: " + playerWeapon);
+
         }
        
 
